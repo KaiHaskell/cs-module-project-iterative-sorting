cdf21649453b21b7ac50a7eaaae1607ad06ab660
Kai Haskell <me@kaihaskell.com>
2020-06-15 17:59:06 -0400

    Iterative_Sorting completed

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -21,9 +21,8 @@ def selection_sort(arr):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-
-    for i in range(len(arr) - 1, 0):
-        for j in i - 1:
+    for i in range(len(arr) - 1, 0, -1):
+        for j in range(0, i):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
 
